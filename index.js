33404396a1756d6ee66b3a026a29035ead265d41
leonardoXLT <148663990+leonardoXLT@users.noreply.github.com>
2024-04-11 20:34:13 +1000

    feat: custom error implemented

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
+const { CustomFancyError } = require('./customError');
 // The above is equivalent to the next two lines:
 // const dotenv = require('dotenv');
 // dotenv.config();
 const pokemonPrinterFile = require("./pokemonPrinter");
-
+const {pokemonNameFromNumber} = require('./pokemonPrinter');
 
 // import blahblah from 'dotenv';
 
@@ -30,21 +31,30 @@ function app() {
 
 		let n = parseFloat(prompt("What number of Pokemon do you want to see?"));
 
-		console.log(typeof(n));
-		console.log("Input is not a number:" + Number.isNaN(n));
+		// console.log(typeof(n));
+		// console.log("Input is not a number:" + Number.isNaN(n));
 
 		if (Number.isNaN(n)){
-			throw new Error("User did not enter a number!");
+			// throw new Error("User did not enter a number!");
+			throw new CustomFancyError("Custom error for NaN!");
 		}
 
-		console.log("You entered " + n);
+		// Give number to pokemon package and get result
+		// let pokemonName = pokemonPrinterFile.pokemonNameFromNumber(n);
 
-		let userInputToExit = prompt("Would you like to try again?");
+		try {
+			let pokemonName = pokemonNameFromNumber(n);
+			console.log(`Your Pokemon is ${pokemonName}! How exciting!`);
+		} catch (error) {
+			console.log("Try a number between 1 and 1025!");
+		} finally {
+			let userInputToExit = prompt("Would you like to try again?");
 
-		if (userInputToExit == "y"){
-			userWantsToExit = false;
-		} else {
-			userWantsToExit = true;
+			if (userInputToExit == "y"){
+				userWantsToExit = false;
+			} else {
+				userWantsToExit = true;
+			}
 		}
 
 	} while (userWantsToExit == false);
@@ -56,6 +66,7 @@ try {
 } catch (error) {
 	console.log("Gracefully shutting down...");
 	console.log(error.message);
+	console.log(JSON.stringify(error));
 	// Full error obj has stacktrace, users should not see that:
 	// console.log(error);
 }
